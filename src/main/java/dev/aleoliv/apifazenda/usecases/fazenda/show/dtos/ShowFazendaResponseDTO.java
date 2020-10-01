beafeecb640ddb7845191ffefae8ca4d977f6bd1
Alexandre Salvador de Oliveira <alexandresalvadoroliveiradev@gmail.com>
2020-10-01 16:43:57 -0300

    Show Fazenda

---
+++ src/main/java/dev/aleoliv/apifazenda/usecases/fazenda/show/dtos/ShowFazendaResponseDTO.java
@@ -0,0 +1,11 @@
+package dev.aleoliv.apifazenda.usecases.fazenda.show.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class ShowFazendaResponseDTO {
+
+	private String nome;
+}
