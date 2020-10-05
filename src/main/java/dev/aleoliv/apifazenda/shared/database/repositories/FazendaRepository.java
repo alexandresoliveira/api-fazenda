b2dc8e48fb64572be2af3ebd9c027e92fc902e22
Alexandre Salvador de Oliveira <alexandresalvadoroliveiradev@gmail.com>
2020-10-05 08:45:39 -0300

    Buscar fazendas pelo nome

--- src/main/java/dev/aleoliv/apifazenda/shared/database/repositories/FazendaRepository.java
+++ src/main/java/dev/aleoliv/apifazenda/shared/database/repositories/FazendaRepository.java
@@ -1,5 +1,6 @@
 package dev.aleoliv.apifazenda.shared.database.repositories;
 
+import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -8,4 +9,5 @@ import dev.aleoliv.apifazenda.shared.database.entities.FazendaEntity;
 
 public interface FazendaRepository extends JpaRepository<FazendaEntity, UUID> {
 
+	Optional<FazendaEntity> findByNome(String nome);
 }
